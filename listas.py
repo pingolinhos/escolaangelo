c9ba3909fd9ef18f6d7601876fe2b836644496d0
Deborah Estefany Remido Santos <162989793+pingolinhos@users.noreply.github.com>
2024-04-17 08:29:42 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,11 @@
+listaNomes = []
+
+listaNomes.append("deborah")
+listaNomes.append("bisdó")
+listaNomes.append("pãozinho")
+
+print(listaNomes[1])
+
+for nome in listaNomes:
+    print (nome)
+    
