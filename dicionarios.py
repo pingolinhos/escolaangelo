c80d465b02c41da8ed3d00563f9caa7c90746e4c
Deborah Estefany Remido Santos <162989793+pingolinhos@users.noreply.github.com>
2024-04-24 08:28:18 -0300

    Add files via upload

---
+++ dicionarios.py
@@ -0,0 +1,25 @@
+listaNomes = []
+
+
+listaNomes.append("Aylla")
+listaNomes.append("Geovana")
+listaNomes.append("Maria Aparecida")
+
+print(listaNomes[1])
+
+for nome in listaNomes:
+    print(nome)
+
+# DICIONÁRIOS
+
+primeiroDicionario = {}
+
+primeiroDicionario['nome'] = "Geovana"
+primeiroDicionario ['tipo'] = 'aluna'
+primeiroDicionario ['numero'] = 9
+print(primeiroDicionario)
+print(primeiroDicionario["tipo"])
+
+#ANATOMIA
+# lista[indice] = Valor
+# dicionario[chave] = Valor
